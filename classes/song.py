e11978a2f554032eb9fc9e8357b57b49c8c8bf03
Stanley Houcke <stanhoucke@gmail.com>
2020-11-27 12:53:37 +0000

    Song - passed attribute tests

--- classes/song.py
+++ classes/song.py
@@ -1,4 +1,6 @@
 class Song():
 
-    def __init__(self):
-        pass
+    def __init__(self, input_song_title, input_artist_name, input_length_seconds):
+        self.song_title = input_song_title
+        self.artist_name = input_artist_name
+        self.length_seconds = input_length_seconds
--- tests/song_test.py
+++ tests/song_test.py
@@ -4,4 +4,15 @@ from classes.song import Song
 class TestSong(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.song_1 = Song("Shake It Off", "Taylor Swift", 219)
+
+    # Class attribute tests
+    def test_song_has_title(self):
+        self.assertEqual("Shake It Off", self.song_1.song_title)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Taylor Swift", self.song_1.artist_name)
+
+    def test_song_has_length(self):
+        self.assertEqual(219, self.song_1.length_seconds)
+    
