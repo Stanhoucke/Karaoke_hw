88b5ef4423a1f646418aa4ca3517c49263d768f2
Stanley Houcke <stanhoucke@gmail.com>
2020-11-27 13:44:16 +0000

    Room - passed attribute tests

--- classes/room.py
+++ classes/room.py
@@ -1,4 +1,6 @@
 class Room():
 
-    def __init__(self):
-        pass
+    def __init__(self, input_room_number):
+        self.room_number = input_room_number
+        self.playlist = []
+        self.guests = []
--- tests/room_test.py
+++ tests/room_test.py
@@ -4,4 +4,13 @@ from classes.room import Room
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.room_1 = Room(1)
+
+    def test_room_has_number(self):
+        self.assertEqual(1, self.room_1.room_number)
+
+    def test_room_has_empty_playlist(self):
+        self.assertEqual(0, len(self.room_1.playlist))
+
+    def test_room_has_no_guests(self):
+        self.assertEqual(0, len(self.room_1.guests))
