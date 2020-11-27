436adddbd92f71ee734b565e044c6ab4605aa259
Stanley Houcke <stanhoucke@gmail.com>
2020-11-27 14:07:11 +0000

    Room - passed tests for add_guest

--- classes/room.py
+++ classes/room.py
@@ -7,3 +7,9 @@ class Room():
 
     def add_song(self, song):
         self.playlist.append(song)
+
+    def add_guest(self, guest):
+        self.guests.append(guest)
+
+    def remove_guest(self, guest):
+        pass
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,6 +1,7 @@
 import unittest
 from classes.room import Room
 from classes.song import Song
+from classes.guest import Guest
 
 class TestRoom(unittest.TestCase):
 
@@ -8,6 +9,8 @@ class TestRoom(unittest.TestCase):
         self.room_1 = Room(1)
 
         self.song_1 = Song("Shake It Off", "Taylor Swift", 219)
+
+        self.guest_1 = Guest("Alice", 38)
 
     # Class attribute tests
     def test_room_has_number(self):
@@ -29,3 +32,15 @@ class TestRoom(unittest.TestCase):
         self.room_1.add_song(self.song_1)
         self.room_1.add_song(self.song_1)
         self.assertEqual(3, len(self.room_1.playlist))
+
+    # Methods that affect number of guests in room
+    def test_add_guest_to_room(self):
+        self.room_1.add_guest(self.guest_1)
+        self.assertEqual(1, len(self.room_1.guests))
+    
+    def test_add_guest_4_times__returns_guests_len_4(self):
+        self.room_1.add_guest(self.guest_1)
+        self.room_1.add_guest(self.guest_1)
+        self.room_1.add_guest(self.guest_1)
+        self.room_1.add_guest(self.guest_1)
+        self.assertEqual(4, len(self.room_1.guests))
